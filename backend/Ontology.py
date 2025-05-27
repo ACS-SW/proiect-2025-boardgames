d3d77db6cba35b65a8da73ab8d6d90cffb91c763
IvanovF <ivanovflorin2@yahoo.com>
2025-05-28 02:05:20 +0300

    third presentation

--- backend/Ontology.py
+++ backend/Ontology.py
@@ -1,17 +1,17 @@
+import os
 from rdflib import Graph, Namespace, RDF, RDFS, OWL, Literal, XSD
 
-# Definirea namespace-ului ontologiei
 BG = Namespace("http://example.org/boardgames#")
-
 g = Graph()
 
-# Clase principale
-classes = ["Game", "Mechanic", "Domain", "PlayerCount", "Duration", "Complexity"]
+classes = [
+    "Game", "Mechanic", "Domain",
+    "PlayerCountCategory", "DurationCategory", "ComplexityCategory", "Designer"
+]
 
 for cls in classes:
     g.add((BG[cls], RDF.type, OWL.Class))
 
-# Proprietăți principale (data properties și object properties)
 properties = {
     "hasMechanic": OWL.ObjectProperty,
     "hasDomain": OWL.ObjectProperty,
@@ -24,10 +24,34 @@ properties = {
     "publishedYear": OWL.DatatypeProperty,
     "ownedByUsers": OWL.DatatypeProperty,
     "ratedByUsers": OWL.DatatypeProperty,
+    "hasPlayerCountCategory": OWL.ObjectProperty,
+    "hasDurationCategory": OWL.ObjectProperty,
+    "hasComplexityCategory": OWL.ObjectProperty,
+    "hasDesigner": OWL.ObjectProperty,
+    "hasExpansion": OWL.ObjectProperty,
+    "isExpansionOf": OWL.ObjectProperty
 }
 
 for prop, prop_type in properties.items():
     g.add((BG[prop], RDF.type, prop_type))
+    g.add((BG[prop], RDFS.label, Literal(prop.replace("_", " "), datatype=XSD.string)))
 
-# Salvare structură ontologie
-g.serialize("data/boardgame_ontology.owl", format="xml")
+player_count_categories = ["Solo", "Couple", "Group", "Party"]
+for category in player_count_categories:
+    uri = BG[category]
+    g.add((uri, RDF.type, BG.PlayerCountCategory))
+    g.add((uri, RDFS.label, Literal(category)))
+
+duration_categories = ["Short", "Medium", "Long", "Epic"]
+for category in duration_categories:
+    uri = BG[category]
+    g.add((uri, RDF.type, BG.DurationCategory))
+    g.add((uri, RDFS.label, Literal(category)))
+
+complexity_categories = ["Very_Easy", "Easy", "Medium", "Hard"]
+for category in complexity_categories:
+    uri = BG[category]
+    g.add((uri, RDF.type, BG.ComplexityCategory))
+    g.add((uri, RDFS.label, Literal(category.replace("_", " "))))
+
+g.serialize("../onto/boardgame_ontology.owl", format="xml")
